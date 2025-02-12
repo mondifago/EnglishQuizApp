24d2331d1cb3be379adba31b7ad95503320910ce
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-02-13 01:49:27 +1100

    DbContext added

---
+++ Data/EnglishQuizAppContext.cs
@@ -0,0 +1,27 @@
+﻿using EnglishQuizApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnglishQuizApp.Data
+{
+    public class EnglishQuizAppContext : DbContext
+    {
+        public EnglishQuizAppContext(DbContextOptions<EnglishQuizAppContext> options) : base(options) { }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Quiz> Quizzes { get; set; }
+        public DbSet<GameSession> GameSessions { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<AnswerOption> AnswerOptions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Ensures CorrectAnswerId is a foreign key (optional but recommended)
+            modelBuilder.Entity<Question>()
+                .HasOne<AnswerOption>()
+                .WithMany()
+                .HasForeignKey(q => q.CorrectAnswerId);
+        }
+    }
+}
