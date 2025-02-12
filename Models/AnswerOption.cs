c48dd84dec8ab81061963a7de5c7b3d6067486d3
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-02-13 01:05:26 +1100

    AnswerOption Class Created

---
+++ Models/AnswerOption.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnglishQuizApp.Models
+{
+    public class AnswerOption
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required(ErrorMessage = "Answer text is required")]
+        public string Text { get; set; }
+    }
+}
