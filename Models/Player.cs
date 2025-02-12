0ba2d677dd32a6880d04fa7f8f38119fb74bba72
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-02-13 00:44:48 +1100

    Added Models folder and created Player Class

---
+++ Models/Player.cs
@@ -0,0 +1,25 @@
+﻿namespace EnglishQuizApp.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Player
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(20, ErrorMessage = "Name can't be longer than 20 characters")]
+        public string Name { get; set; }
+
+        [Range(5, 100, ErrorMessage = "Age must be between 5 and 100")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int TotalScore { get; set; } = 0;
+    }
+}
+
