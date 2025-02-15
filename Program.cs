bdaae13707f9d742d46300b49f9efb94cd0fd21c
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-02-15 22:38:04 +1100

    CRUD pages for Player Class created with scaffolding

--- Program.cs
+++ Program.cs
@@ -3,8 +3,12 @@ using EnglishQuizApp.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<EnglishQuizAppContext>(options =>
+builder.Services.AddDbContextFactory<EnglishQuizAppContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 
 // Add services to the container.
@@ -19,6 +23,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
